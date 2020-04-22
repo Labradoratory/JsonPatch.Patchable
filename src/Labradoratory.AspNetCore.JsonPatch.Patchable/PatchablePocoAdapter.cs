948baef1c0bbeb40dff6238e39aca9f287250662
Ed Downs <ed@labradoratory.com>
2020-04-22 10:49:01 -0700

    Adds PatchableChildren attribute.
    PatchableChildren allows child properties to be patched, while stopping the property itself from being patched via replace or remove.

--- src/Labradoratory.AspNetCore.JsonPatch.Patchable/PatchablePocoAdapter.cs
+++ src/Labradoratory.AspNetCore.JsonPatch.Patchable/PatchablePocoAdapter.cs
@@ -18,19 +18,43 @@ namespace Labradoratory.AspNetCore.JsonPatch.Patchable
         {
             if (!base.TryTraverse(target, segment, contractResolver, out value, out errorMessage))
                 return false;
-            
+
             // If the value is a list, we need to check that the property has a PatchableAttribute.
-            if(value is IList)
+            if (value.GetType().IsClass && !(value is string))
             {
-                // No need to check this, if we get here this already worked.
-                TryGetJsonProperty(target, contractResolver, segment, out JsonProperty jsonProperty);
-                // If the property is not writable, we'll just do a fake add so we can generate the correct message.
+                // If the property is not patchable, we'll just do a fake add so we can generate the correct message.
                 // Using the embeded resources file is a PITA and not much more efficient.
-                if (!jsonProperty.Writable)
+                if (!CanPatchChildren(target, segment, contractResolver))
                     return TryAdd(target, segment, contractResolver, value, out errorMessage);
             }
 
             return true;
+        }
+
+        /// <summary>
+        /// Determines whether the children of the object, at the property identified by the segment name,
+        /// can be patched.
+        /// </summary>
+        /// <param name="target">The target.</param>
+        /// <param name="segment">The segment.</param>
+        /// <param name="contractResolver">The contract resolver.</param>
+        /// <returns>
+        ///   <c>true</c> if [is patchable list] [the specified target]; otherwise, <c>false</c>.
+        /// </returns>
+        protected virtual bool CanPatchChildren(object target, string segment, IContractResolver contractResolver)
+        {
+            if (contractResolver.ResolveContract(target.GetType()) is JsonObjectContract jsonObjectContract)
+            {
+                var pocoProperty = jsonObjectContract
+                    .Properties
+                    .FirstOrDefault(p => string.Equals(p.PropertyName, segment, StringComparison.OrdinalIgnoreCase));
+
+                // Property must have PatchableAttribute or patch operation cannot be applied.
+                if (pocoProperty.AttributeProvider.GetAttributes(false).Any(a => a is PatchableAttribute || a is PatchableChildrenAttribute))
+                    return true;
+            }
+
+            return false;
         }
 
         protected override bool TryGetJsonProperty(object target, IContractResolver contractResolver, string segment, out JsonProperty jsonProperty)
--- test/Labradoratory.AspNetCore.JsonPatch.Patchable.Test/PatchablePocoAdapter_Tests.cs
+++ test/Labradoratory.AspNetCore.JsonPatch.Patchable.Test/PatchablePocoAdapter_Tests.cs
@@ -12,7 +12,7 @@ namespace Labradoratory.AspNetCore.JsonPatch.Patchable.Test
         [Fact]
         public void TryGetJsonProperty_NotWritableWhenMissingPatchable()
         {
-            var target = new TestAdapter();
+            var target = new Test_PatchablePocoAdapter();
             var test = new Test();
 
             target.Test_TryGetJsonProperty(test, new DefaultContractResolver(), "StringValue1", out var jsonProperty);
@@ -22,7 +22,7 @@ namespace Labradoratory.AspNetCore.JsonPatch.Patchable.Test
         [Fact]
         public void TryGetJsonProperty_WritableWhenPatchable()
         {
-            var target = new TestAdapter();
+            var target = new Test_PatchablePocoAdapter();
             var test = new Test();
 
             target.Test_TryGetJsonProperty(test, new DefaultContractResolver(), "StringValue2", out var jsonProperty);
@@ -32,11 +32,70 @@ namespace Labradoratory.AspNetCore.JsonPatch.Patchable.Test
         [Fact]
         public void TryGetJsonProperty_NotWritableWhenNotWritable()
         {
-            var target = new TestAdapter();
+            var target = new Test_PatchablePocoAdapter();
             var test = new Test();
 
             target.Test_TryGetJsonProperty(test, new DefaultContractResolver(), "StringValue3", out var jsonProperty);
             Assert.False(jsonProperty.Writable);
+        }
+
+        [Fact]
+        public void TryGetJsonProperty_TrueWhenPoco()
+        {
+            var target = new Test_PatchablePocoAdapter();
+            var test = new Test();
+
+            var result = target.Test_TryGetJsonProperty(test, new DefaultContractResolver(), "StringValue3", out var jsonProperty);
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void TryGetJsonProperty_ReturnsFalseWhenNotPoco()
+        {
+            var target = new Test_PatchablePocoAdapter();
+            var test = new List<string>();
+
+            var result = target.Test_TryGetJsonProperty(test, new DefaultContractResolver(), "StringValue3", out var jsonProperty);
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void TryGetJsonProperty_IListMarkedPatchableChildrenNotPatchable()
+        {
+            var target = new Test_PatchablePocoAdapter();
+            var test = new Test();
+
+            target.Test_TryGetJsonProperty(test, new DefaultContractResolver(), "Dictionary1", out var jsonProperty);
+            Assert.False(jsonProperty.Writable);
+        }
+
+        [Fact]
+        public void TryTraverse_IListMarkedPatchableChildrenCanTraverse()
+        {
+            var target = new PatchablePocoAdapter();
+            var test = new Test();
+
+            var result = target.TryTraverse(test, "Dictionary1", new DefaultContractResolver(), out var value, out var errorMessage);
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void TryTraverse_IListMarkedPatchableCanTraverse()
+        {
+            var target = new PatchablePocoAdapter();
+            var test = new Test();
+
+            var result = target.TryTraverse(test, "Dictionary2", new DefaultContractResolver(), out var value, out var errorMessage);
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void TryTraverse_IListNotMarkedPatchableOrPatchableChildrenCanNotTraverse()
+        {
+            var target = new PatchablePocoAdapter();
+            var test = new Test();
+            var result = target.TryTraverse(test, "Dictionary3", new DefaultContractResolver(), out var value, out var errorMessage);
+            Assert.False(result);
         }
 
         private class Test
@@ -47,9 +106,17 @@ namespace Labradoratory.AspNetCore.JsonPatch.Patchable.Test
             public string StringValue2 { get; set; }
 
             public string StringValue3 { get; }
+
+            [PatchableChildren]
+            public Dictionary<string, string> Dictionary1 { get; set; } = new Dictionary<string, string> { { "1", "value1" } };
+
+            [Patchable]
+            public Dictionary<string, string> Dictionary2 { get; set; } = new Dictionary<string, string> { { "2", "value2" } };
+
+            public Dictionary<string, string> Dictionary3 { get; set; } = new Dictionary<string, string> { { "3", "value3" } };
         }
 
-        private class TestAdapter : PatchablePocoAdapter
+        private class Test_PatchablePocoAdapter : PatchablePocoAdapter
         {
             public bool Test_TryGetJsonProperty(object target, IContractResolver contractResolver, string segment, out JsonProperty jsonProperty)
             {
